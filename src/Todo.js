7fdd5ad5dee7ebab2f3f72a6debb9903d296fd56
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-11-16 22:24:23 +0200

    Create toggleTodo and editTodo with hooks

--- src/Todo.js
+++ src/Todo.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import useToggleState from './hooks/useToggleState';
+import EditTodoForm from './EditTodoForm';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Checkbox from '@material-ui/core/Checkbox';
@@ -7,27 +9,35 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 
-function Todo({ task, completed, removeTodo, id, toggleTodo }) {
+function Todo({ task, completed, removeTodo, id, toggleTodo, editTodo }) {
+	const [isEditing, toggle] = useToggleState(false);
+
 	return (
 		<ListItem>
-			<Checkbox
-				tabIndex={-1}
-				checked={completed}
-				onClick={() => toggleTodo(id)}
-			/>
-			<ListItemText
-				style={{ textDecoration: completed ? 'line-through' : 'none' }}
-			>
-				{task}
-			</ListItemText>
-			<ListItemSecondaryAction>
-				<IconButton aria-lable='Delete' onClick={() => removeTodo(id)}>
-					<DeleteIcon />
-				</IconButton>
-				<IconButton aria-lable='Edit'>
-					<EditIcon />
-				</IconButton>
-			</ListItemSecondaryAction>
+			{isEditing ? (
+				<EditTodoForm editTodo={editTodo} id={id} task={task} toggle={toggle} />
+			) : (
+				<Fragment>
+					<Checkbox
+						tabIndex={-1}
+						checked={completed}
+						onClick={() => toggleTodo(id)}
+					/>
+					<ListItemText
+						style={{ textDecoration: completed ? 'line-through' : 'none' }}
+					>
+						{task}
+					</ListItemText>
+					<ListItemSecondaryAction>
+						<IconButton aria-lable='Delete' onClick={() => removeTodo(id)}>
+							<DeleteIcon />
+						</IconButton>
+						<IconButton aria-lable='Edit' onClick={toggle}>
+							<EditIcon />
+						</IconButton>
+					</ListItemSecondaryAction>
+				</Fragment>
+			)}
 		</ListItem>
 	);
 }
