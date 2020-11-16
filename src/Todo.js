91eff0f2a1f1fc7047b721045067bd3e1ce32458
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-11-16 20:59:23 +0200

    Create removeTodo and toggleTodo with hooks

--- src/Todo.js
+++ src/Todo.js
@@ -7,17 +7,21 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 
-function Todo({ task, completed }) {
+function Todo({ task, completed, removeTodo, id, toggleTodo }) {
 	return (
 		<ListItem>
-			<Checkbox tabIndex={-1} checked={completed} />
+			<Checkbox
+				tabIndex={-1}
+				checked={completed}
+				onClick={() => toggleTodo(id)}
+			/>
 			<ListItemText
 				style={{ textDecoration: completed ? 'line-through' : 'none' }}
 			>
 				{task}
 			</ListItemText>
 			<ListItemSecondaryAction>
-				<IconButton aria-lable='Delete'>
+				<IconButton aria-lable='Delete' onClick={() => removeTodo(id)}>
 					<DeleteIcon />
 				</IconButton>
 				<IconButton aria-lable='Edit'>
