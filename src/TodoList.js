f067abfbc4fc1badb989ed4ae358cf0c9fdb95a2
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-11-18 15:54:27 +0200

    Setup localStorage with useEffect hook

--- src/TodoList.js
+++ src/TodoList.js
@@ -5,26 +5,28 @@ import Divider from '@material-ui/core/Divider';
 import Todo from './Todo';
 
 function TodoList({ todos, removeTodo, toggleTodo, editTodo }) {
-    return (
-        <Paper>
-            <List>
-                {todos.map((todo, index) => (
-                    <Fragment>
-                        <Todo
-                            id={todo.id}
-                            task={todo.task}
-                            key={todo.id}
-                            completed={todo.completed}
-                            removeTodo={removeTodo}
-                            toggleTodo={toggleTodo}
-                            editTodo={editTodo}
-                        />
-                        {index < todos.length - 1 && <Divider />}
-                    </Fragment>
-                ))}
-            </List>
-        </Paper>
-    );
+	if (todos.length)
+		return (
+			<Paper>
+				<List>
+					{todos.map((todo, index) => (
+						<Fragment>
+							<Todo
+								id={todo.id}
+								task={todo.task}
+								key={todo.id}
+								completed={todo.completed}
+								removeTodo={removeTodo}
+								toggleTodo={toggleTodo}
+								editTodo={editTodo}
+							/>
+							{index < todos.length - 1 && <Divider />}
+						</Fragment>
+					))}
+				</List>
+			</Paper>
+		);
+	return null;
 }
 
 export default TodoList;
